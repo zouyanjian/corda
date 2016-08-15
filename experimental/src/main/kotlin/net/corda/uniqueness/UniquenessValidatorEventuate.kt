e8c81cce125b251f9ee60e70f67bae60f622c417
Ross Nicoll <ross.nicoll@r3cev.com>
2016-08-15 17:55:02 +0100

    Adapt Akka state machine code to more recent version of Eventuate

--- experimental/src/main/kotlin/net/corda/uniqueness/UniquenessValidatorEventuate.kt
+++ experimental/src/main/kotlin/net/corda/uniqueness/UniquenessValidatorEventuate.kt
@@ -1,18 +1,19 @@
 package com.r3corda.uniqueness
 
-import akka.japi.Procedure
+import akka.actor.ActorRef
+import akka.japi.pf.ReceiveBuilder
 import akka.persistence.SnapshotOffer
 import com.r3corda.protocols.twoparty.Coordinator
 import com.rbmhtechnology.eventuate.AbstractEventsourcedActor
 import java.io.Serializable
 import java.util.*
 
-class UniquenessValidatorEventuate : AbstractEventsourcedActor() {
-    internal class UniqueTxnReqCmd(val uniquenessRef: Int, val inputStates : List<Int>) : Serializable {
+class UniquenessValidatorEventuate(eventLog: ActorRef) : AbstractEventsourcedActor("uniqueness-validator", eventLog) {
+    internal class UniqueTxnReqCmd(val uniquenessRef: Int, val inputStates: List<Int>) : Serializable {
         private val serialVersionUID = 1L;
     }
 
-    internal class UniqueTxnEvt(val inputStates : List<Int>) : Serializable {
+    internal class UniqueTxnEvt(val inputStates: List<Int>) : Serializable {
         private val serialVersionUID = 1L;
     }
 
@@ -23,31 +24,31 @@ class UniquenessValidatorEventuate : AbstractEventsourcedActor() {
             consumedStates.addAll(evt.inputStates)
         }
 
-        fun copy() : ValidatorState {
+        fun copy(): ValidatorState {
             return ValidatorState(HashSet<Int>(consumedStates))
         }
     }
 
-    override fun onReceiveCommand(msg: Any) {
-        if (msg is UniqueTxnReqCmd) {
-            // first validate the request - that none of the incoming states have previously been consumed
-            if (msg.inputStates.intersect(validatorState.consumedStates).isEmpty()) {
-                // create an event based on the new states - this can be replayed at time of recovery
-                val event = UniqueTxnEvt(msg.inputStates)
-                // persist the event
-                persist(event, object : Procedure<UniqueTxnEvt> {
-                    @Throws(Exception::class)
-                    override fun apply(evt: UniqueTxnEvt) {
-                        validatorState.update(evt)
-                        // broadcast event on eventstream
-                        getContext().system().eventStream().publish(evt)
-                    }
-                })
-                // tell the sender that the states were added successfully
-                sender.tell(Coordinator.UniqueTxnResCmd(msg.uniquenessRef), self)
+    init {
+        onReceiveCommand(ReceiveBuilder
+                .match(UniqueTxnReqCmd::class.java, { msg: UniqueTxnReqCmd -> doUniqueTransaction(msg) })
+                .match(String::class.java, { msg: String -> doStringCommand(msg) })
+                .build())
+        onReceiveEvent(ReceiveBuilder
+                .match(UniqueTxnEvt::class.java, { msg -> validatorState.update(msg) })
+                .match(SnapshotOffer::class.java, { msg -> validatorState = msg.snapshot() as ValidatorState })
+                .build())
+    }
+
+    private fun doStringCommand(msg: String) {
+        if (msg.equals("snap")) {
+            save(validatorState.copy()) { metadata, err ->
+                if (err == null) {
+                    // Tell the sender we succeeded
+                } else {
+                    // Tell the sender we failed
+                }
             }
-        } else if (msg.equals("snap")) {
-            saveSnapshot(validatorState.copy())
         } else if (msg.equals("print")) {
             println(validatorState)
         } else {
@@ -55,19 +56,20 @@ class UniquenessValidatorEventuate : AbstractEventsourcedActor() {
         }
     }
 
-    override fun onReceiveRecover(msg: Any) {
-        // mechanism for loading all previously generated events into actor state
-        if (msg is UniqueTxnEvt) {
-            validatorState.update(msg)
-        } else if (msg is SnapshotOffer) {
-            validatorState = msg.snapshot() as ValidatorState
-        } else {
-            unhandled(msg)
+    private fun doUniqueTransaction(msg: UniqueTxnReqCmd) {
+        // first validate the request - that none of the incoming states have previously been consumed
+        if (msg.inputStates.intersect(validatorState.consumedStates).isEmpty()) {
+            // create an event based on the new states - this can be replayed at time of recovery
+            val event = UniqueTxnEvt(msg.inputStates)
+            // persist the event
+            persist(event) { evt, error ->
+                validatorState.update(evt)
+                // broadcast event on eventstream
+                context().system().eventStream().publish(evt)
+            }
+            // tell the sender that the states were added successfully
+            sender().tell(Coordinator.UniqueTxnResCmd(msg.uniquenessRef), self())
         }
-    }
-
-    override fun persistenceId(): String {
-        return "uniqueness-validator";
     }
 
     private var validatorState = ValidatorState()
