a42ab2c1b426ec26df5e3c2785022ca08ff0c316
Maksymilian Pawlak <120831+m4ksio@users.noreply.github.com>
2017-09-04 14:53:22 +0100

    [WIP] Separate Spring webapp

---
+++ samples/irs-demo-spring/cordapp/src/test/kotlin/net/corda/irs/Main.kt
@@ -0,0 +1,31 @@
+package net.corda.irs
+
+import net.corda.core.internal.concurrent.transpose
+import net.corda.core.node.services.ServiceInfo
+import net.corda.core.utilities.getOrThrow
+import net.corda.testing.DUMMY_BANK_A
+import net.corda.testing.DUMMY_BANK_B
+import net.corda.testing.DUMMY_NOTARY
+import net.corda.irs.api.NodeInterestRates
+import net.corda.node.services.transactions.SimpleNotaryService
+import net.corda.testing.driver.driver
+
+/**
+ * This file is exclusively for being able to run your nodes through an IDE (as opposed to running deployNodes)
+ * Do not use in a production environment.
+ */
+fun main(args: Array<String>) {
+    driver(dsl = {
+        val (controller, nodeA, nodeB) = listOf(
+                startNode(DUMMY_NOTARY.name, setOf(ServiceInfo(SimpleNotaryService.type), ServiceInfo(NodeInterestRates.Oracle.type))),
+                startNode(DUMMY_BANK_A.name),
+                startNode(DUMMY_BANK_B.name)
+        ).transpose().getOrThrow()
+
+        startWebserver(controller)
+        startWebserver(nodeA)
+        startWebserver(nodeB)
+
+        waitForAllNodesToFinish()
+    }, useTestClock = true, isDebug = true)
+}
---
+++ samples/irs-demo-spring/web/src/main/kotlin/net/corda/irs/web/IrsDemoWebApplication.kt
@@ -0,0 +1,36 @@
+package net.corda.irs.web
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import net.corda.client.jackson.JacksonSupport
+import net.corda.client.rpc.CordaRPCClient
+import net.corda.core.messaging.CordaRPCOps
+import net.corda.core.utilities.parseNetworkHostAndPort
+import net.corda.finance.plugin.registerFinanceJSONMappers
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.DependsOn
+import org.springframework.stereotype.Service
+
+@SpringBootApplication
+class IrsDemoWebApplication {
+
+    @Bean
+    fun rpcClient(): CordaRPCOps {
+        return CordaRPCClient("localhost:10006".parseNetworkHostAndPort()).start("user", "password").proxy
+    }
+
+    @Bean
+//    @DependsOn("rcp")
+    fun objectMapper(@Autowired cordaRPCOps: CordaRPCOps): ObjectMapper {
+        val mapper = JacksonSupport.createDefaultMapper(cordaRPCOps)
+        registerFinanceJSONMappers(mapper)
+        return mapper
+    }
+}
+
+fun main(args: Array<String>) {
+    SpringApplication.run(IrsDemoWebApplication::class.java, *args)
+}
