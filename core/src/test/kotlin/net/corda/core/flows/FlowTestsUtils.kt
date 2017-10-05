a0e03296992d35bd52e9e45f2f92875076234c50
sollecitom <michele.sollecito@gmail.com>
2017-10-05 15:39:50 +0100

    [CORDA-683] Documented types in FlowTestsUtils.

--- core/src/test/kotlin/net/corda/core/flows/FlowTestsUtils.kt
+++ core/src/test/kotlin/net/corda/core/flows/FlowTestsUtils.kt
@@ -5,8 +5,14 @@ import net.corda.node.internal.InitiatedFlowFactory
 import net.corda.node.internal.StartedNode
 import kotlin.reflect.KClass
 
+/**
+ * Allows to simplify writing flows that simply rend a message back to an initiating flow.
+ */
 class Answer<out R : Any>(session: FlowSession, override val answer: R, closure: (result: R) -> Unit = {}) : SimpleAnswer<R>(session, closure)
 
+/**
+ * Allows to simplify writing flows that simply rend a message back to an initiating flow.
+ */
 abstract class SimpleAnswer<out R : Any>(private val session: FlowSession, private val closure: (result: R) -> Unit = {}) : FlowLogic<Unit>() {
     @Suspendable
     override fun call() {
@@ -18,11 +24,17 @@ abstract class SimpleAnswer<out R : Any>(private val session: FlowSession, priva
     protected abstract val answer: R
 }
 
+/**
+ * A flow that does not do anything when triggered.
+ */
 class NoAnswer(private val closure: () -> Unit = {}) : FlowLogic<Unit>() {
     @Suspendable
     override fun call() = closure()
 }
 
+/**
+ * Allows to register a flow of type [R] against an initiating flow of type [I].
+ */
 inline fun <I : FlowLogic<*>, reified R : FlowLogic<*>> StartedNode<*>.registerInitiatedFlow(initiatingFlowType: KClass<I>, crossinline construct: (session: FlowSession) -> R) {
     internals.internalRegisterFlowFactory(initiatingFlowType.java, InitiatedFlowFactory.Core { session -> construct(session) }, R::class.java, true)
 }
