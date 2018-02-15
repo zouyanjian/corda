da1b66550c1db0cec4167cabd5e2d4a9f11a0323
Konstantinos Chalkias <konstantinos.chalkias@r3.com>
2018-02-15 12:38:09 +0000

    fixes after rebase

--- core/src/test/kotlin/net/corda/core/crypto/TransactionSignatureTest.kt
+++ core/src/test/kotlin/net/corda/core/crypto/TransactionSignatureTest.kt
@@ -1,7 +1,7 @@
 package net.corda.core.crypto
 
 import net.corda.core.serialization.serialize
-import net.corda.testing.SerializationEnvironmentRule
+import net.corda.testing.core.SerializationEnvironmentRule
 import org.junit.Rule
 import org.junit.Test
 import kotlin.test.assertEquals
@@ -133,23 +133,23 @@ class TransactionSignatureTest {
         val txID = testBytes.sha256()
 
         // Test for ECDSA-R1, using a wrong Metadata scheme (intended for ECDSA K1).
-        val keyPairECDSA_R1 = Crypto.generateKeyPair(Crypto.ECDSA_SECP256R1_SHA256)
-        assertEquals(3, Crypto.findSignatureScheme(keyPairECDSA_R1.public).schemeNumberID)
+        val keyPairECDSAR1 = Crypto.generateKeyPair(Crypto.ECDSA_SECP256R1_SHA256)
+        assertEquals(3, Crypto.findSignatureScheme(keyPairECDSAR1.public).schemeNumberID)
 
         // Create a SignableData object.
-        val k1_schemeNumberID = 2
-        val signableDataForK1 = SignableData(txID, SignatureMetadata(1, k1_schemeNumberID))
+        val k1SchemeNumberID = 2
+        val signableDataForK1 = SignableData(txID, SignatureMetadata(1, k1SchemeNumberID))
 
         // Sign the meta object.
-        assertFailsWith<IllegalArgumentException> { keyPairECDSA_R1.sign(signableDataForK1) }
+        assertFailsWith<IllegalArgumentException> { keyPairECDSAR1.sign(signableDataForK1) }
 
         // Test for EdDSA ed25519, using a wrong Metadata scheme (intended for RSA).
         val keyPairEdDSA = Crypto.generateKeyPair(Crypto.EDDSA_ED25519_SHA512)
         assertEquals(4, Crypto.findSignatureScheme(keyPairEdDSA.public).schemeNumberID)
 
         // Create a SignableData object.
-        val rsa_schemeNumberID = 1
-        val signableDataEdDSA = SignableData(txID, SignatureMetadata(1, rsa_schemeNumberID))
+        val rsaSchemeNumberID = 1
+        val signableDataEdDSA = SignableData(txID, SignatureMetadata(1, rsaSchemeNumberID))
 
         // Sign the meta object.
         assertFailsWith<IllegalArgumentException> { keyPairEdDSA.sign(signableDataEdDSA) }
@@ -160,25 +160,25 @@ class TransactionSignatureTest {
         val txID = testBytes.sha256()
 
         // Test for ECDSA-R1, using a wrong Metadata scheme (intended for ECDSA K1).
-        val keyPairECDSA_R1 = Crypto.generateKeyPair(Crypto.ECDSA_SECP256R1_SHA256)
+        val keyPairECDSAR1 = Crypto.generateKeyPair(Crypto.ECDSA_SECP256R1_SHA256)
 
         // Create a SignableData object.
-        val k1_schemeNumberID = 2
-        val signableDataForK1 = SignableData(txID, SignatureMetadata(1, k1_schemeNumberID))
+        val k1SchemeNumberID = 2
+        val signableDataForK1 = SignableData(txID, SignatureMetadata(1, k1SchemeNumberID))
 
         // Sign the meta object.
         // Not using keyPair.sign(signableData) or doSign(keyPair, signableData) because
         // it won't let me sign a wrong Metadata object; we are only testing verification of potentially malicious Metadata.
-        val signatureBytesR1 = Crypto.doSign(Crypto.findSignatureScheme(keyPairECDSA_R1.private), keyPairECDSA_R1.private, signableDataForK1.serialize().bytes)
-        val txSignatureR1 = TransactionSignature(signatureBytesR1, keyPairECDSA_R1.public, signableDataForK1.signatureMetadata)
+        val signatureBytesR1 = Crypto.doSign(Crypto.findSignatureScheme(keyPairECDSAR1.private), keyPairECDSAR1.private, signableDataForK1.serialize().bytes)
+        val txSignatureR1 = TransactionSignature(signatureBytesR1, keyPairECDSAR1.public, signableDataForK1.signatureMetadata)
         assertFailsWith<IllegalArgumentException> { Crypto.doVerify(txID, txSignatureR1) }
 
         // Test for EdDSA ed25519, using a wrong Metadata scheme (intended for RSA).
         val keyPairEdDSA = Crypto.generateKeyPair(Crypto.EDDSA_ED25519_SHA512)
 
         // Create a SignableData object.
-        val rsa_schemeNumberID = 1
-        val signableDataEdDSA = SignableData(txID, SignatureMetadata(1, rsa_schemeNumberID))
+        val rsaSchemeNumberID = 1
+        val signableDataEdDSA = SignableData(txID, SignatureMetadata(1, rsaSchemeNumberID))
 
         // Sign the meta object.
         val signatureBytesEdDSA = Crypto.doSign(Crypto.findSignatureScheme(keyPairEdDSA.private), keyPairEdDSA.private, signableDataEdDSA.serialize().bytes)
