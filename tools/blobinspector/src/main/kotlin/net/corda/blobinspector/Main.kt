a5a218b9334fede59146332a91f5925eaf728190
Katelyn Baker <katelyn.baker@r3.com>
2018-02-18 21:46:08 +0000

    BLOB INSPECTOR INITIAL COMMIT

---
+++ tools/blobinspector/src/main/kotlin/net/corda/blobinspector/Main.kt
@@ -0,0 +1,30 @@
+package net.corda.blobinspector
+
+import org.apache.commons.cli.*;
+
+fun main(args: Array<String>) {
+    val options = Options()
+
+    val a = Option("a", "aaa", true, "test1")
+    a.isRequired = true
+    options.addOption(a)
+
+    val b = Option("b", "bbb", true, "test 2")
+    b.isRequired = false
+    options.addOption(b)
+
+    val parser = DefaultParser()
+    val formatter = HelpFormatter()
+    val cmd: CommandLine
+
+    try {
+        cmd = parser.parse(options, args)
+    } catch (e: ParseException) {
+        System.out.println(e.message)
+        formatter.printHelp("blobinspector", options)
+
+        System.exit(1)
+        return
+    }
+
+}
